d9a5d1d232bafe2bd58a9dfa85e9af05a6225c7e
Elia <ceravolo.elia@gmail.com>
2022-03-31 12:38:04 +0200

    update

--- src/utils/CookieManager.ts
+++ src/utils/CookieManager.ts
@@ -8,7 +8,8 @@ export function getCookies(): number {
   if (document.cookie && document.cookie.includes('theme')) {
     const value = document.cookie.split(';')[0].replace('theme=', '');
     return value.includes('light') ? EMod.LIGHT : EMod.DARK;
-  } else return EMod.NOT_SET;
+  } 
+  return EMod.NOT_SET;
 }
 
 export class CookieManager {}
